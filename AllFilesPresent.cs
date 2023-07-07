6c8b5477c6851b31e63bc3e5ad032b5797ff4132
thines01 <thines01@yahoo.com>
2023-07-06 19:19:46 -0500

    Add project files.

---
+++ AllFilesPresent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AllFilesPresent
+{
+   public class CAllFilesPresent
+   {
+      public static bool AllFilesPresent(string[] args, ref string strError)
+      {
+         KeyValuePair<string, bool> kvp_s2blnResult =
+         (
+            from s in args
+            let blnMissing = !File.Exists(s)
+            where blnMissing
+            select new KeyValuePair<string, bool>(s, blnMissing)
+         ).FirstOrDefault();
+
+         strError = kvp_s2blnResult.Key;
+         return !kvp_s2blnResult.Value;
+      }
+   }
+}
